7e8c42b98b0f899666163d71fa3a4120d5ca8665
Prince <this.is.kumar.prince@gmail.com>
2021-11-18 10:58:15 +0530

    variables completed

--- 4.variables/src/main.rs
+++ 4.variables/src/main.rs
@@ -1,3 +1,31 @@
+// Error
+// fn main() {
+//     let x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
+// const CONST: u32 = 90;
+// // let CONST=90;//Error
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+//     println!("The value of CONST is : {}", CONST);
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x is: {}", x);
+    }
+    println!("The value of x is: {}", x);
+    let mut x = "Print";
+    println!("The value of x is: {}", x);
+    x = "Hello Bro";
+    println!("The value of x is: {}", x);
 }
