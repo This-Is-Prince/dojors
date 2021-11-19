b14f4a5cd4ec0f0771ee2ddecec602ba9fa07966
Prince <this.is.kumar.prince@gmail.com>
2021-11-19 13:17:30 +0530

    basic odd even program complete

--- 7.control_flow/src/main.rs
+++ 7.control_flow/src/main.rs
@@ -1,9 +1,34 @@
+// fn main() {
+//     let n = 6;
+//     let m = 7;
+//     if n < m {
+//         println!("n is less than {}", m);
+//     } else {
+//         println!("n is greater than {}", m);
+//     }
+// }
+
+use std::io;
+
 fn main() {
-    let n = 6;
-    let m = 7;
-    if n < m {
-        println!("n is less than {}", m);
-    } else {
-        println!("n is greater than {}", m);
+    let mut input = String::new();
+    loop {
+        println!("Please enter a number: ");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to get input");
+        let input_number: i32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("Error is: {}", err);
+                continue;
+            }
+        };
+        input = String::new();
+        if input_number % 2 == 0 {
+            println!("Input is Even");
+        } else {
+            println!("Input is Odd");
+        }
     }
 }
