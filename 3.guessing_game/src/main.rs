a88a925b4115164cbc8652bf7069a4f257845a16
Prince <this.is.kumar.prince@gmail.com>
2021-11-23 10:20:23 +0530

    3.guessing_game practice done

--- 3.guessing_game/src/main.rs
+++ 3.guessing_game/src/main.rs
@@ -1,13 +1,41 @@
+// use rand::Rng;
+// use std::cmp::Ordering;
+// use std::io;
+
+// fn main() {
+//     println!("<----- Guessing Game ----->");
+//     let secret_number = rand::thread_rng().gen_range(1..=100);
+//     loop {
+//         println!("Enter a guess:");
+//         // let secret_number = rand::thread_rng().gen_range(1..101);
+//         let mut guess = String::new();
+//         io::stdin()
+//             .read_line(&mut guess)
+//             .expect("Failed to get input");
+//         let guess: u32 = match guess.trim().parse() {
+//             Ok(num) => num,
+//             Err(_) => continue,
+//         };
+//         match guess.cmp(&secret_number) {
+//             Ordering::Less => println!("Too Low!"),
+//             Ordering::Greater => println!("Too High!"),
+//             Ordering::Equal => {
+//                 println!("You Win!");
+//                 break;
+//             }
+//         }
+//     }
+// }
+
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("<----- Guessing Game ----->");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("<--- Guessing Game --->");
+    let secret_number = rand::thread_rng().gen_range(1..101);
     loop {
-        println!("Enter a guess:");
-        // let secret_number = rand::thread_rng().gen_range(1..101);
+        println!("Enter a guess : ");
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -17,10 +45,10 @@ fn main() {
             Err(_) => continue,
         };
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too Low!"),
+            Ordering::Less => println!("Too low!"),
             Ordering::Greater => println!("Too High!"),
             Ordering::Equal => {
-                println!("You Win!");
+                println!("You win!");
                 break;
             }
         }
