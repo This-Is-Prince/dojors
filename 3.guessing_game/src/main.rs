31b4bb8ddd463a49aa008aee28e68665d672ac1e
Prince <this.is.kumar.prince@gmail.com>
2021-11-27 13:16:39 +0530

    guessing-game practice done

--- 3.guessing_game/src/main.rs
+++ 3.guessing_game/src/main.rs
@@ -27,15 +27,43 @@
 //     }
 // }
 
+// use rand::Rng;
+// use std::cmp::Ordering;
+// use std::io;
+
+// fn main() {
+//     println!("<--- Guessing Game --->");
+//     let secret_number = rand::thread_rng().gen_range(1..101);
+//     loop {
+//         println!("Enter a guess : ");
+//         let mut guess = String::new();
+//         io::stdin()
+//             .read_line(&mut guess)
+//             .expect("Failed to get input");
+//         let guess: u32 = match guess.trim().parse() {
+//             Ok(num) => num,
+//             Err(_) => continue,
+//         };
+//         match guess.cmp(&secret_number) {
+//             Ordering::Less => println!("Too low!"),
+//             Ordering::Greater => println!("Too High!"),
+//             Ordering::Equal => {
+//                 println!("You win!");
+//                 break;
+//             }
+//         }
+//     }
+// }
+
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("<--- Guessing Game --->");
+    println!("<--- Welcome , let's play guessing game --->");
     let secret_number = rand::thread_rng().gen_range(1..101);
     loop {
-        println!("Enter a guess : ");
+        println!("Please enter your guess :-> ");
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -46,11 +74,11 @@ fn main() {
         };
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too low!"),
-            Ordering::Greater => println!("Too High!"),
+            Ordering::Greater => println!("Too high!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You Win");
                 break;
             }
-        }
+        };
     }
 }
