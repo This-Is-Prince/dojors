7a5ff6ec35a1697783518eaa9ab2b42da73174ce
Prince <this.is.kumar.prince@gmail.com>
2021-12-11 08:58:57 +0530

    functions completed

--- lets_get_rusty/3.common_programming_concepts/src/main.rs
+++ lets_get_rusty/3.common_programming_concepts/src/main.rs
@@ -1,3 +1,67 @@
 fn main() {
-    println!("Hello, world!");
+    // Can't assign x twice because x is immutable
+    // let x=5;
+    // println!("The value of x is : {}",x);
+    // x = 6;
+    // println!("The value of x is : {}",x);
+
+    // mut keyword for mutable variables
+    // let mut x = 5;
+    // println!("The value of x is : {}", x);
+    // x = 6;
+    // println!("The value of x is : {}", x);
+
+    // Const variable
+    // const SUBSCRIBER_COUNT: u32 = 100_000;
+    // println!("The value of SUBSCRIBER_COUNT is {}", SUBSCRIBER_COUNT);
+
+    // Shadowing
+    // let x = 5;
+    // println!("The value of x is : {}", x);
+    // let x = "six";
+    // println!("The value of x is : {}", x);
+
+    // Data Types
+    // let a: i32 = 98_222;
+
+    // let f: u8 = 255;
+
+    // let e: f64 = 2.2;
+
+    // let t: bool = true;
+
+    // Character
+    // let c: char = 'z';
+
+    // TUple
+    // let tup: (&str, i32) = ("Let's Get Rusty!", 100_000);
+    // // println!("The value of tup.0 is {}", tup.0);
+    // // println!("The value of tup.1 is {}", tup.1);
+    // let (channel, sub_count) = tup;
+    // println!("The value of channel is {}", channel);
+    // println!("The value of sub_count is {}", sub_count);
+
+    // Arrays
+    // let error_codes: [i32; 3] = [200, 404, 500];
+    // let not_found = error_codes[1];
+    // let byte = [0; 8];
+
+    // FUnctions
+    // my_function()
+    // my_function2(1, 2);
+    println!("The value of return is {}", my_function_return(1, 2));
+}
+
+// fn my_function() {
+//     println!("Another Function.");
+// }
+// fn my_function2(x: i32, y: i32) {
+//     println!("The value of x is : {}", x);
+//     println!("The value of y is : {}", y);
+// }
+fn my_function_return(x: i32, y: i32) -> i32 {
+    println!("The value of x is : {}", x);
+    println!("The value of y is : {}", y);
+    // return x + y;
+    x + y
 }
