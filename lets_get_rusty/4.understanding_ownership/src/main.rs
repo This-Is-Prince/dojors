3a88e70e96e3102a2c52e1a31b91d23813f48eff
Prince <this.is.kumar.prince@gmail.com>
2021-12-16 08:46:54 +0530

    ownership and functions completed

--- lets_get_rusty/4.understanding_ownership/src/main.rs
+++ lets_get_rusty/4.understanding_ownership/src/main.rs
@@ -29,9 +29,38 @@ fn main() {
 
     // println!("{}, world!", s1); // Error
 
-    let s1 = String::from("Hello");
-    let s2 = s1.clone(); // Cloning
+    // let s1 = String::from("Hello");
+    // let s2 = s1.clone(); // Cloning
 
-    println!("s1 - > {}, world!", s1);
-    println!("s2 - > {}, world!", s2);
+    // println!("s1 - > {}, world!", s1);
+    // println!("s2 - > {}, world!", s2);
+
+    /*
+    Ownership and Functions
+    */
+    // let s = String::from("hello");
+    // takes_ownership(s);
+    // println!("{}", s); //Error s is not value because s move in takes_ownership function
+
+    // let x = 5;
+    // makes_copy(x);
+    // println!("{}", x);
+
+    let s1 = gives_ownership();
+    let s2 = String::from("hello2");
+    let s3 = takes_and_gives_back(s2);
+    println!("s1 = {} , s3 = {}", s1, s3);
+}
+// fn takes_ownership(some_string: String) {
+//     println!("{}", some_string);
+// }
+// fn makes_copy(some_integer: i32) {
+//     println!("{}", some_integer);
+// }
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
