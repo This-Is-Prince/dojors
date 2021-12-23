c5a6afc2dc71683b8befef02eef86d7645c6629f
Prince <this.is.kumar.prince@gmail.com>
2021-12-23 08:24:15 +0530

    core data types completed

--- The_Rust_Programming_Language/2.Types_And_Variables/2.02.Core_Data_Types/src/main.rs
+++ The_Rust_Programming_Language/2.Types_And_Variables/2.02.Core_Data_Types/src/main.rs
@@ -1,3 +1,44 @@
+use std::mem;
+
 fn main() {
-    println!("Hello, world!");
+    // // unsigned 0 .. 255
+    // let a: u8 = 123; // 8bits
+
+    // // signed -128..127
+    // let i: i8 = 123; // 8bits
+    // println!("a = {}", a);
+
+    // // by default rust variables are immutable
+    // // a = 456;
+
+    // // we can use mut keyword for mutable variable
+    // let mut b: i8 = 0; // mutable
+    // println!("b = {}", b);
+    // b = 42;
+    // println!("b = {}", b);
+    // let mut c = 123456789; // 32-bit signed i32
+    // println!("c = {}, size = {} bytes", c, mem::size_of_val(&c));
+    // c = 1234567890;
+    // println!("c = {}", c);
+
+    // i8, u8, i16, u16, 132, u32, i64, u64
+    let z: isize = 123; // isize / usize
+    let size_of_z = mem::size_of_val(&z);
+    println!(
+        "z = {}, takes up {} bytes, {}-bit os",
+        z,
+        size_of_z,
+        size_of_z * 8
+    );
+
+    let d = 'x';
+    println!("d = {}, size = {} bytes", d, mem::size_of_val(&d));
+
+    let e = 2.5; // double-precision
+    println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
+    // true false
+    let g = false;
+    println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+    let f = 4 > 0; //true
+    println!("f = {}, size = {} bytes", f, mem::size_of_val(&f));
 }
