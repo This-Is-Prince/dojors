7eca5ccb2a5a55c832b8b41815767cf32977fb12
Prince <this.is.kumar.prince@gmail.com>
2022-01-14 12:17:42 +0530

    7-Rust_Functions_and_Procedures completed

--- Rust_Tutorials/7-Rust_Functions_and_Procedures/src/main.rs
+++ Rust_Tutorials/7-Rust_Functions_and_Procedures/src/main.rs
@@ -1,24 +1,37 @@
 #[allow(unused_variables)]
+
 fn main() {
-    let returned_data = some_function(5.1, 50);
-    println!("returned_data is {}", returned_data);
-    // Hello();
-    some_procedure(2.3, 1);
+    // some_function(2.1, 2);
+
+    some_procedure(1.1, 1);
     some_str_procedure("test");
 
-    let string_slice_var = "Howdy!";
+    let string_slice_var: &str = "Howdy!";
     some_str_procedure(string_slice_var);
 
-    let string_var = String::from("I'm a Real String :)");
+    let string_var = String::from("I'm a REAL String :)");
     some_str_procedure(&string_var);
-    some_string_procedure(string_var);
+    some_str_procedure(&string_var);
+
+    // some_string_procedure(string_var);
+    // some_string_procedure(string_var); // Error
+
+    // OR
+    some_string_procedure2(&string_var);
+    some_string_procedure2(&string_var);
 }
 
-fn some_string_procedure(param: String) {
-    println!("I'm in some_str_procedure with param {}", param)
+fn some_string_procedure2(param: &String) {
+    println!("I'm in some_string_procedure with param {}", param);
 }
+
+#[allow(dead_code)]
+fn some_string_procedure(param: String) {
+    println!("I'm in some_string_procedure with param {}", param);
+}
+
 fn some_str_procedure(param: &str) {
-    println!("I'm in some_str_procedure with param {}", param)
+    println!("I'm in some_str_procedure with param {}", param);
 }
 
 fn some_procedure(param_a: f32, param_b: i8) {
@@ -26,25 +39,17 @@ fn some_procedure(param_a: f32, param_b: i8) {
 }
 
 #[allow(dead_code)]
-fn some_function(param_a: f32, _param_b: i128) -> f32 {
-    println!("I'm in some_function!");
-    // 10.1 // No semicolon means this is what's returned by the function
-    // 10f32
-    // 10 as f32
-    // 10.
-
-    // let return_var = 10.1 * param_a + _param_b as f32;
-    // return_var
+fn some_function(param_a: f32, param_b: i128) -> f32 {
+    println!("some_function");
+    // 10.1f32
+    // 10.1
+    // 10.1 as f32
 
     if param_a < 100. {
-        let return_var = 10.1 * param_a + _param_b as f32;
-        return_var
+        let return_ar = 10.1 * param_a + param_b as f32;
+        // return return_ar;
+        return_ar
     } else {
         -1.
     }
 }
-
-// #[allow(non_snake_case)]
-// fn Hello() {
-//     println!("hello")
-// }
