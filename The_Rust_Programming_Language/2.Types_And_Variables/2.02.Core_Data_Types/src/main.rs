3956103be7f5dea32b0d39dea806c49fbf6227ab
Prince <this.is.kumar.prince@gmail.com>
2022-01-15 18:53:44 +0530

    data types revisie completed

--- The_Rust_Programming_Language/2.Types_And_Variables/2.02.Core_Data_Types/src/main.rs
+++ The_Rust_Programming_Language/2.Types_And_Variables/2.02.Core_Data_Types/src/main.rs
@@ -1,27 +1,28 @@
 use std::mem;
 
 fn main() {
-    // // unsigned 0 .. 255
-    // let a: u8 = 123; // 8bits
+    // unsigned 0.. 255
+    let a: u8 = 123; // 8bits
+    println!("a = {}", a);
+    // a=456;
 
-    // // signed -128..127
-    // let i: i8 = 123; // 8bits
-    // println!("a = {}", a);
+    // mut
+    let mut b: i8 = 0; // mutable
+    println!("b = {}", b);
+    b = 42;
+    println!("b = {}", b);
 
-    // // by default rust variables are immutable
-    // // a = 456;
+    let mut c = 123456789; // 32-bit signed i32
+    println!("c = {}, size = {}", c, mem::size_of_val(&c));
+    c = -1;
+    println!(
+        "c = {}, after modification size = {}",
+        c,
+        mem::size_of_val(&c)
+    );
 
-    // // we can use mut keyword for mutable variable
-    // let mut b: i8 = 0; // mutable
-    // println!("b = {}", b);
-    // b = 42;
-    // println!("b = {}", b);
-    // let mut c = 123456789; // 32-bit signed i32
-    // println!("c = {}, size = {} bytes", c, mem::size_of_val(&c));
-    // c = 1234567890;
-    // println!("c = {}", c);
+    //i8 u8, i16 u16, i32 u32, i64 u64
 
-    // i8, u8, i16, u16, 132, u32, i64, u64
     let z: isize = 123; // isize / usize
     let size_of_z = mem::size_of_val(&z);
     println!(
@@ -31,14 +32,16 @@ fn main() {
         size_of_z * 8
     );
 
-    let d = 'x';
-    println!("d = {}, size = {} bytes", d, mem::size_of_val(&d));
+    let d: char = 'x';
+    println!("d = {}, takes up {} bytes", d, mem::size_of_val(&d));
 
-    let e = 2.5; // double-precision
-    println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
+    // float
+    let e = 2.5; // double-precision, 8 bytes or 64 bits, f64
+    println!("e = {}, takes up {} bytes", e, mem::size_of_val(&e));
+
     // true false
     let g = false;
-    println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+    println!("g = {}, takes up {} bytes", g, mem::size_of_val(&g));
     let f = 4 > 0; //true
-    println!("f = {}, size = {} bytes", f, mem::size_of_val(&f));
+    println!("f = {}, takes up {} bytes", f, mem::size_of_val(&f));
 }
