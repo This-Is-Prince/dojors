ed25bc92466a7e9e365532d72ad138030bdb11a2
Prince <this.is.kumar.prince@gmail.com>
2022-01-16 09:26:09 +0530

    option and if let while let revised

--- The_Rust_Programming_Language/4.Data_Structures/4.04.OptionT_and_if_let_while_let/src/main.rs
+++ The_Rust_Programming_Language/4.Data_Structures/4.04.OptionT_and_if_let_while_let/src/main.rs
@@ -1,21 +1,19 @@
 fn main() {
     let x = 3.0;
     let y = 2.0;
-    // Option -> Some(v) | None
+
+    // Option -> Some(z) / None
     let result = if y != 0.0 { Some(x / y) } else { None };
-    match result {
-        Some(z) => println!("{}/{}={}", x, y, z),
-        None => println!("can't divide by zero"),
-    }
 
     if let Some(z) = result {
-        println!("result = {}", z);
+        println!("value of z is {}", z);
     }
+
     let mut x = 0;
     while let Some(z) = result {
-        println!("z = {}, x = {}", z, x);
+        println!("x = {}, z = {}", x, z);
         x += 1;
-        if x == 10 {
+        if x > 10 {
             break;
         }
     }
