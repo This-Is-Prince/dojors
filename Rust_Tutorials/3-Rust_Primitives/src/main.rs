282a3c7a5e5c28b423276fb61cc3f0b0505597b6
Prince <this.is.kumar.prince@gmail.com>
2022-02-11 17:12:54 +0530

    rust basic primitives types revised

--- Rust_Tutorials/3-Rust_Primitives/src/main.rs
+++ Rust_Tutorials/3-Rust_Primitives/src/main.rs
@@ -1,41 +1,41 @@
-#[allow(unused_variables)] // prevent from warning related to unused variables.
-#[allow(unused_assignments)] // prevent from warning related to unused assignments.
+#[allow(unused_variables)] // Allow unused Variables
+#[allow(unused_assignments)] // Allow Unused Assignments
 
-// variable and function name written acc to snake case
 fn main() {
-    // #[allow(non_snake_case)] // prevent from warning related to non snake case variable or functions name.
-    // let Some_data: bool = true; // or false
     // let some_data: bool = true; // or false
-    // let some_data = true;
-    // by default variable are immutable in rust
-    // some_data=false;// error because some_data is not immutable if we want to mutate it we must use mut keyword in front of some_data variable at the time of creation of variable
+    // let some_data = false; // compiler can infer data types
+    // let mut some_data = false;
+    // some_data = true;
 
-    // unsigned integer u8 = 2 ^ N = 2 ^ 8. = 256
-    // signed integer i8 = 2 ^ (N - 1) - 1 = 2 ^ 7 - 1. = 128
-    // let some_data: u8 = 10; // 0 to 255
-    // let some_data: i8 = 10; // -128 to +127
-    // println!("Min i8 is {}", std::i8::MIN);
-    // println!("Max i8 is {}", std::i8::MAX);
-
-    // let test = some_data + 120; // Panic integer overflow
-    // println!("test is {}", test);
+    // let mut some_data: i8 = 10;
+    // some_data = 127; // i8 integer range from -128 to 127
+    // let test = some_data + 120; // Error
+    // println!("MIN i8 is {}", std::i8::MIN);
+    // println!("MAX i8 is {}", std::i8::MAX);
 
     // let some_data: u8 = 10; // from 0 to 255
-    // let some_other_data = 255 + some_data; // Error!!! Too big
-    // let some_other_data2 = 1 - some_data; // Error!!! Too small
+    // println!("MIN u8 is {}", std::u8::MIN);
+    // println!("MAX u8 is {}", std::u8::MAX);
 
-    // let some_data: i128 = 10; // from -WOW to +WOW - 1
-    // println!("Min i128 is {}", std::i128::MIN);
-    // println!("Max i128 is {}", std::i128::MAX);
+    // i8, i16, i32, i64, i128
+    // u8, u16, u32, u64, u128
+    // println!("MIN u128 is {}", std::u128::MIN);
+    // println!("MAX u128 is {}", std::u128::MAX);
 
-    // let some_isize: isize = 10; //depends on whether your computer is 32bit or 64bit
-    // let some_usize: usize = 10; //same here
+    // let some_isize: isize = 10; // depends on what ever architecture of computer, 32bit or 64bit
+    // let some_isize: usize = 10; // depends on what ever architecture of computer, 32bit or 64bit
+    // println!("MIN isize is {}", std::isize::MIN);
+    // println!("MAX isize is {}", std::isize::MAX);
 
-    // let some_data: f32 = 10.; //Don't forget the dot somewhere in the computer
-    // println!("some_isize is {}", some_isize);
-    // println!("some_usize is {}", some_usize);
-    // println!("some_data is {}", some_data);
+    // Float
+    // f32, f64
+    // let some_data: f32 = 10.; // dot is necessary in float number
+    // let some_data = 10.2; // by default float number is f64 types
 
+    // Char
+    // char
     let some_char: char = 'a';
-    println!("some_char is {}", some_char)
+    println!("MAX char is {}", std::char::MAX);
+    println!("char is {} bytes", std::mem::size_of_val(&some_char));
+    println!("some_char value is : {}", some_char);
 }
