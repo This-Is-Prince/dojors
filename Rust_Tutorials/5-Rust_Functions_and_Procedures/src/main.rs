4ab3049700bf68979394a77e3b22365ce271f901
Prince <this.is.kumar.prince@gmail.com>
2022-02-11 17:48:18 +0530

    functions and procedure revised

--- Rust_Tutorials/5-Rust_Functions_and_Procedures/src/main.rs
+++ Rust_Tutorials/5-Rust_Functions_and_Procedures/src/main.rs
@@ -1,55 +1,57 @@
 #[allow(unused_variables)]
 
 fn main() {
-    // some_function(2.1, 2);
+    println!("-----Rust Functions and Procedures-----");
 
-    some_procedure(1.1, 1);
+    let returned_data = some_function(2.2, 50);
+    println!("returned_data is {}", returned_data);
+
+    some_procedure(2.3, 1);
+
     some_str_procedure("test");
 
-    let string_slice_var: &str = "Howdy!";
+    let string_slice_var: &str = "Prince";
     some_str_procedure(string_slice_var);
 
     let string_var = String::from("I'm a REAL String :)");
     some_str_procedure(&string_var);
     some_str_procedure(&string_var);
+    some_str_procedure(&string_var);
+    some_str_procedure(&string_var);
 
     // some_string_procedure(string_var);
-    // some_string_procedure(string_var); // Error
+    // some_string_procedure(string_var); // Error string_var is not available it is moved
 
-    // OR
+    some_string_procedure2(&string_var);
+    some_string_procedure2(&string_var);
     some_string_procedure2(&string_var);
     some_string_procedure2(&string_var);
 }
 
+#[allow(dead_code)]
 fn some_string_procedure2(param: &String) {
     println!("I'm in some_string_procedure with param {}", param);
 }
-
 #[allow(dead_code)]
 fn some_string_procedure(param: String) {
     println!("I'm in some_string_procedure with param {}", param);
 }
 
+#[allow(dead_code)]
 fn some_str_procedure(param: &str) {
     println!("I'm in some_str_procedure with param {}", param);
 }
 
+#[allow(dead_code)]
 fn some_procedure(param_a: f32, param_b: i8) {
     println!("I'm in some_procedure with a {} b {}", param_a, param_b);
 }
 
 #[allow(dead_code)]
+#[allow(unused_variables)]
 fn some_function(param_a: f32, param_b: i128) -> f32 {
-    println!("some_function");
-    // 10.1f32
-    // 10.1
-    // 10.1 as f32
-
-    if param_a < 100. {
-        let return_ar = 10.1 * param_a + param_b as f32;
-        // return return_ar;
-        return_ar
-    } else {
-        -1.
-    }
+    println!("I'm in some_function!");
+    // 3.23 // No semicolon means this is what's returned by the function
+    let return_var = 10. * param_a + param_b as f32;
+    return_var
 }
