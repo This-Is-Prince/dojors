9de543a8b069706066d723158a1618aa3e97df63
Prince <this.is.kumar.prince@gmail.com>
2022-02-11 17:35:54 +0530

    string and string slices revised

--- Rust_Tutorials/4-Rust_String_vs_str_slices/src/main.rs
+++ Rust_Tutorials/4-Rust_String_vs_str_slices/src/main.rs
@@ -1,47 +1,48 @@
 #[allow(unused_variables)]
-#[allow(unused_assignments)]
-
 fn main() {
-    let example_str: &str = "Howdy";
-    let example_string: String = String::from("Partner");
+    println!("------Rust String vs str (Slices)------");
 
-    // translate
+    let example_str: &str = "Prince";
+    let example_string: String = String::from("Prince");
+
     let string_from_str: String = example_str.to_string();
     let string_from_str2: String = "Some hardcoded string".to_string();
 
-    let string_from_hardcoded: String = String::from("Some hardcoded");
-    let string_from_str_var: String = String::from(example_str);
+    let string_from_hardcoded = String::from("Some hardcoded");
+    let string_from_str_var = String::from(example_str);
 
     let str_from_string: &str = &example_string;
 
-    // combined strings
-    // let test="first"+"second"; // Error
-    let combine_string_literals: String = ["first", " second"].concat();
+    // let test = "first" + "second"; // Error
+    let combine_string_literals: String = ["first", "second"].concat();
     let combine_with_format_macro: String = format!("{} {}", "first", "second");
 
-    // let string_plus_str = example_str + example_string; // Error
-    let string_plus_str = example_string + example_str;
+    let string_plus_str = example_string + example_str; // First 'String' than '&str'
 
     let mut mut_string = String::new();
     mut_string.push_str(example_str);
     mut_string.push_str("Some hardcoded literal");
-    let hi = String::from("HI There!");
-    // mut_string.push_str(hi); // Error
-    mut_string.push_str(&hi);
-    mut_string.push('m');
-    // mut_string.push("m"); // Error require single quote
+    mut_string.push('a');
+    // mut_string.push("m"); // Error
 
     let a = String::from("a");
     let b = String::from("b");
     let combined = a + &b + &mut_string;
 
-    let str_from_substring: &str = &example_str[0..2]; // 2 is not included
-    let str_from_substring: &str = &hi[0..=1]; // &hi[..2] same
-    let str_from_substring: &str = &hi[..]; // &hi[0..hi.len()] same
-                                            // let str_from_string: &str = &example_str[0..200]; // This will ERROR
+    let str_from_substring: &str = &example_str[0..2];
+    let str_from_substring: &str = &example_str[..2];
+    let str_from_substring: &str = &example_str[..];
+    let str_from_substring: &str = &example_str[1..];
+    let str_from_substring: &str = &example_str[1..=2];
 
-    let char_by_index = example_str.chars().nth(1);
-    if let Some(nth_char) = char_by_index {
-        println!("nth char is: {}", nth_char);
+    // let char_by_index=&example_str[1]; // error
+    let char_by_index = &example_str.chars().nth(1); // return Option<char>
+    if let Some(ch) = char_by_index {
+        println!("value of ch is : {}", ch);
+    }
+
+    match char_by_index {
+        Some(ch) => println!("value of ch is : {}", ch),
+        None => println!("no character"),
     }
 }
