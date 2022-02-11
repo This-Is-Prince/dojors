ade65414111dc3022ad4dd236498656c8aab907c
Prince <this.is.kumar.prince@gmail.com>
2022-02-11 17:58:24 +0530

    conditional statements revised

--- Rust_Tutorials/6-Rust_Conditional_Statements/src/main.rs
+++ Rust_Tutorials/6-Rust_Conditional_Statements/src/main.rs
@@ -1,28 +1,29 @@
 #[allow(unused_variables)]
-
+#[allow(dead_code)]
 fn main() {
+    println!("-----Rust Conditional Statements-----");
+
     let some_bool = true;
     let some_int = 30;
-    let some_int2 = 50;
 
-    // if some_bool == true {
-    //     println!("Hit If branch");
-    // }
-    // if some_bool {
-    //     println!("Hit If branch");
-    // } else {
-    //     println!("Hit Else branch");
-    // }
-
-    if some_bool || (some_int > 100 && some_int2 == 200) {
+    if some_bool == true {
         println!("Hit If branch");
+    } else if some_bool == false && some_int == 30 {
+        println!("Hit If Else branch");
     } else {
-        println!("Hit Else branch");
+        println!("Hit else branch");
     }
 
-    let var_from_inline = if some_int == 9 { 300 } else { 400 };
-    println!("var_from_inline is {}", var_from_inline);
+    let var_from_inline = if some_int == 9 {
+        300
+    } else if some_int == 30 {
+        println!("This is if else branch");
+        30
+    } else {
+        400
+    };
 
+    // Match Statements
     match some_bool {
         true => {
             println!("Hit true branch");
@@ -31,34 +32,11 @@ fn main() {
             println!("Hit false branch");
         }
     }
-
     match some_int {
         0 => println!("Hit 0 branch"),
-        1..=100 => println!("Between 1 and 100"),
+        1 | 2 => println!("1 or 2"),
+        tmp @ 5..=30 => println!("value of tmp is {}", tmp),
+        31..=100 => println!("Between 31 and 100 branch"),
         _ => println!("Else branch"),
     }
-
-    match some_int {
-        0 => {
-            println!("Hit 0 branch")
-        }
-        1..=100 => {
-            println!("Between 1 and 100")
-        }
-        _ => {
-            println!("Else branch")
-        }
-    }
-
-    let var_from_match = match some_bool {
-        true => 10,
-        false => 20,
-    };
-    println!("{}", var_from_match);
-
-    let var_from_match2 = match some_int {
-        0 => 0,
-        1 | 2 => 100,
-        _ => 200,
-    };
 }
