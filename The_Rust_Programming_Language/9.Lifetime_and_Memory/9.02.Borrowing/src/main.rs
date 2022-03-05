90e8814ae2992a7a0ce28f09911ff04a0c34dafe
Prince <this.is.kumar.prince@gmail.com>
2022-03-06 05:21:06 +0530

    borrowing completed

--- The_Rust_Programming_Language/9.Lifetime_and_Memory/9.02.Borrowing/src/main.rs
+++ The_Rust_Programming_Language/9.Lifetime_and_Memory/9.02.Borrowing/src/main.rs
@@ -1,3 +1,4 @@
+#[allow(unused_mut)]
 fn main() {
     let print_vector = |x: &Vec<i32>| {
         println!("x[0] = {}", x[0]);
@@ -8,8 +9,16 @@ fn main() {
     println!("{:?}", v);
 
     let mut a = 40;
-    let b = &mut a;
-
-    *b += 2;
+    let c = &a;
+    println!("c = {}", c);
+    {
+        let b = &mut a;
+        *b += 2;
+    }
     println!("a = {}", a);
+    let mut z = vec![3, 2, 1];
+    for i in &z {
+        println!("i = {}", i);
+        // z.push(5); // ERROR, cannot borrow `z` as mutable because it is also borrowed as immutable
+    }
 }
