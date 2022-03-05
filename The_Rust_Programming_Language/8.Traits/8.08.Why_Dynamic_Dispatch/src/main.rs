49a80e436eb9c75fcb44d1854ceebf2444b5c324
Prince <this.is.kumar.prince@gmail.com>
2022-03-05 20:24:04 +0530

    why dynamic dispatch completed

---
+++ The_Rust_Programming_Language/8.Traits/8.08.Why_Dynamic_Dispatch/src/main.rs
@@ -0,0 +1,42 @@
+struct Circle {
+    radius: f64,
+}
+
+struct Square {
+    side: f64,
+}
+
+trait Shape {
+    fn area(&self) -> f64;
+}
+
+impl Shape for Square {
+    fn area(&self) -> f64 {
+        self.side * self.side
+    }
+}
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        self.radius * self.radius * std::f64::consts::PI
+    }
+}
+
+fn why_dynamic_dispatch() {
+    let shapes: [&dyn Shape; 4] = [
+        &Circle { radius: 1.0 },
+        &Square { side: 3.0 },
+        &Circle { radius: 2.0 },
+        &Square { side: 4.0 },
+    ];
+
+    for (i, shape) in shapes.iter().enumerate() {
+        println!("Shape #{} has area {}", i, shape.area());
+    }
+}
+
+fn main() {
+    println!("----------Why Dynamic Dispatch----------");
+
+    why_dynamic_dispatch();
+}
