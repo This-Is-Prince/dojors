c71a1ab148f97107eca558f3040a4322a8ec3f0f
Prince <this.is.kumar.prince@gmail.com>
2022-04-18 15:06:37 +0530

    ch31_generics completed

--- easy_rust/ch31_generics/src/main.rs
+++ easy_rust/ch31_generics/src/main.rs
@@ -2,6 +2,27 @@ use std::cmp::PartialOrd;
 use std::fmt::Debug; // Debug is located at std::ftm::Debug. So now we can just write 'Debug'.
 use std::fmt::Display;
 
+fn return_number_with_type_annotation(number: i32) -> i32 {
+    println!("Here is your number.");
+    number
+}
+
+fn return_number_with_generic_type<T>(number: T) -> T {
+    println!("Here is your number.");
+    number
+}
+
+/*
+fn print_number_without_giving_debug_trait<T>(number: T) {
+    println!("Here is your number: {:?}", number); // ERROR, `T` doesn't implement `std::fmt::Debug`
+}
+*/
+
+fn print_number_with_giving_debug_trait<T: Debug>(number: T) {
+    // <T: Debug> is the important part
+    println!("Here is your number: {:?}", number);
+}
+
 #[allow(dead_code)]
 #[derive(Debug)]
 struct Animal {
@@ -12,10 +33,38 @@ struct Animal {
 fn print_item<T: Debug>(item: T) {
     println!("Here is your item: {:?}", item);
 }
-/*
-fn compare_and_display<T:Display,U:Display+PartialOrd>(statement:T,num_1:U,num_2:U){
 
-} */
+fn compare_and_display<T: Display, U: Display + PartialOrd>(statement: T, num_1: U, num_2: U) {
+    println!(
+        "{}: Is {} greater than {}? {}",
+        statement,
+        num_1,
+        num_2,
+        num_1 > num_2
+    );
+}
+
+fn compare_and_display_with_where<T, U>(statement: T, num_1: U, num_2: U)
+where
+    T: Display,
+    U: Display + PartialOrd,
+{
+    println!(
+        "{}: Is {} greater than {}? {}",
+        statement,
+        num_1,
+        num_2,
+        num_1 > num_2
+    );
+}
+
+fn say_two<T: Display, U: Display>(statement_1: T, statement_2: U) {
+    // Type T needs Display, Type U needs Display
+    println!(
+        "I have two things to say: {} and {}",
+        statement_1, statement_2
+    );
+}
 
 #[allow(unused_variables)]
 fn main() {
@@ -38,25 +87,13 @@ fn main() {
     let number = 55;
     print_item(number);
     print_item(charlie);
-}
 
-fn return_number_with_type_annotation(number: i32) -> i32 {
-    println!("Here is your number.");
-    number
-}
+    compare_and_display("Listen up!", 9, 8);
+    compare_and_display_with_where("Listen up!", 90, 890);
 
-fn return_number_with_generic_type<T>(number: T) -> T {
-    println!("Here is your number.");
-    number
-}
-
-/*
-fn print_number_without_giving_debug_trait<T>(number: T) {
-    println!("Here is your number: {:?}", number); // ERROR, `T` doesn't implement `std::fmt::Debug`
-}
-*/
-
-fn print_number_with_giving_debug_trait<T: Debug>(number: T) {
-    // <T: Debug> is the important part
-    println!("Here is your number: {:?}", number);
+    say_two("Hello there!", String::from("I hate sand.")); // Type T is a &str, but type U is String
+    say_two(
+        String::from("Where is Abohar?"),
+        String::from("Is she all right?"),
+    ); // Both types are String.
 }
