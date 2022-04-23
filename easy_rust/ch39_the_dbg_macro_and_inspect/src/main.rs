f2615d5667c533e024aaae895f44a327d812629e
Prince <this.is.kumar.prince@gmail.com>
2022-04-23 19:33:40 +0530

    the dbg! macro and .inspect chapter completed

---
+++ easy_rust/ch39_the_dbg_macro_and_inspect/src/main.rs
@@ -0,0 +1,41 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+fn main() {
+    println!("The dbg! macro and .inspect");
+    /*
+    let my_number = 8;
+    dbg!(my_number);
+
+    let mut my_number = dbg!(9);
+
+    dbg!(my_number += 10);
+    let new_vec = dbg!(vec![8, 9, 10]);
+    let double_vec = dbg!(new_vec.iter().map(|x| x * 2).collect::<Vec<i32>>());
+    dbg!(double_vec);
+    */
+
+    let new_vec = vec![8, 9, 10];
+
+    /*
+    let double_vec = new_vec
+    .iter()
+    .inspect(|first_item| println!("The item is: {}", first_item))
+    .map(|x| x * 2)
+    .inspect(|next_item| println!("Then it is: {}", next_item))
+    .collect::<Vec<i32>>();
+    */
+
+    let double_vec = new_vec
+        .iter()
+        .inspect(|first_item| {
+            println!("The item is: {}", first_item);
+            match **first_item % 2 {
+                // first item is a &&i32 so we use **
+                0 => println!("It is even."),
+                _ => println!("It is odd."),
+            }
+            println!("In binary it is {:b}.", first_item);
+        })
+        .map(|x| x * 2)
+        .collect::<Vec<i32>>();
+}
