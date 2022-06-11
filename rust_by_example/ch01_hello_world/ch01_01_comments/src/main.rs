b7fa79a1cccc276e55a0a02a38ed0b0054638103
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-06-11 19:27:58 +0530

    comments chapter completed

---
+++ rust_by_example/ch01_hello_world/ch01_01_comments/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // line comments which go to the end of the line.
+    /* block comments which go to the closing delimiter. */
+    println!("Hello, world!");
+    let x = 5+/* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x)
+}
