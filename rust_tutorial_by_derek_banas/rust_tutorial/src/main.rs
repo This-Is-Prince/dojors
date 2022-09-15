4427322b0a8881da75d5f01ad50d38d79114795f
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-09-15 18:30:26 +0530

    feat: hello world completed

--- rust_tutorial_by_derek_banas/rust_tutorial/src/main.rs
+++ rust_tutorial_by_derek_banas/rust_tutorial/src/main.rs
@@ -1,3 +1,16 @@
+#![allow(unused)]
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io::{self, Read};
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
 fn main() {
+    println!("What is your name?");
+    let mut name = String::new();
+    let greeting = "Nie to meet you";
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Didn't Receive Input");
     println!("Hello, world!");
 }
