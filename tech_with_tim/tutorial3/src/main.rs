64d8aa9c5cf84f6a0b48eb6dc7be465356cd74d8
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-09-15 19:21:07 +0530

    feat: variables and constant revised

--- tech_with_tim/tutorial3/src/main.rs
+++ tech_with_tim/tutorial3/src/main.rs
@@ -1,42 +1,15 @@
 fn main() {
-    println!("Tutorial 3 - Variables, Constants and Shadowing.");
-    // let x = 4;
-    // println!("x is: {}", x);
-
-    // x = 5; // ERROR, cannot assign twice to immutable variable `x`
-    // println!("x is: {}", x);
-
-    // let mut x = 4;
-    // println!("x is: {}", x);
-
-    // x = 5; // ERROR, cannot assign twice to immutable variable `x`
-    // println!("x is: {}", x);
-
-    // let x = 4;
-    // println!("x is: {}", x);
-    // let x = 5;
-    // println!("x is: {}", x);
-
-    // let x = 4;
-    // println!("x is: {}", x);
-
-    // {
-    //     let x = x - 2;
-    //     println!("x is: {}", x);
-    // }
-
-    // let x = x + 1;
-    // println!("x is: {}", x);
-
     let x = 4;
-    println!("x is: {}", x);
+    println!("x is :{}", x);
+    let x: u32 = 5;
+    println!("x is :{}", x);
 
-    let x = "hello";
-    println!("x is: {}", x);
+    // x=5; //
+    let mut x: u32 = 6;
+    println!("x is :{}", x);
+    x = 76;
+    println!("x is :{}", x);
 
     const SECONDS_IN_MINUTE: u32 = 60;
-    println!("SECONDS_IN_MINUTE is: {}", SECONDS_IN_MINUTE);
-
-    // const SECONDS_IN_MINUTE: u32 = 60; // ERROR, the name `SECONDS_IN_MINUTE` is defined multiple times
-    // println!("SECONDS_IN_MINUTE is: {}", SECONDS_IN_MINUTE);
+    println!("{}", SECONDS_IN_MINUTE)
 }
