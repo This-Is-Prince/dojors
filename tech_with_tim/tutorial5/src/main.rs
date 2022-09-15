5bf238e722f4e6ee017a2967ae4692f94a64824c
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-09-15 19:44:39 +0530

    feat: getting console input revised

--- tech_with_tim/tutorial5/src/main.rs
+++ tech_with_tim/tutorial5/src/main.rs
@@ -1,18 +1,11 @@
 use std::io;
 
 fn main() {
-    println!("Tutorial 5 - Console Input");
+    println!("Hello World!");
     let mut input = String::new();
-
-    if let Ok(value) = io::stdin().read_line(&mut input) {
-        println!("total no of bytes read: {}", value);
-        println!("input value is: {}", input);
-    } else {
-        println!("Failed to read line")
-    }
 
     io::stdin()
         .read_line(&mut input)
-        .expect("Failed to read line");
-    println!("input value is: {}", input);
+        .expect("failed to read line");
+    println!("{}", input)
 }
