d600ee24156432abde07b18fbe8c3a59fe8c83b0
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-04-15 17:17:10 +0530

    feat: ch09 type inference complete

---
+++ easy_rust1/ch09_type_inference/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+
+    let small_number: u8 = 10;
+    let small_number = 10u8;
+    let small_number = 10_u8;
+    let big_number = 100_000_000i32;
+    let number = 0_____u8;
+    let number2 = 1___5___223___u32;
+    println!("number2: {}", number2);
+    let my_float = 5.;
+    let my_float: f64 = 5.0;
+    let my_other_float: f32 = 8.5;
+    let third_float = my_float as f32 + my_other_float;
+    
+}
