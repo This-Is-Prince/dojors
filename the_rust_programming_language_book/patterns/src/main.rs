a27684fd672b86fb13753cde6c0c0ebfde1f39ac
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-07-15 20:36:56 +0530

    feat: rust patterns from 11-15

--- the_rust_programming_language_book/patterns/src/main.rs
+++ the_rust_programming_language_book/patterns/src/main.rs
@@ -253,6 +253,144 @@ fn pattern10(run: bool) {
     println!();
 }
 
+fn pattern11(run: bool) {
+    if !run {
+        return;
+    }
+    println!("-------------------- Pattern 11: --------------------\n");
+
+    let row: i32 = 11;
+    let column: i32 = (row + 1) / 2;
+    let mut condition: bool;
+
+    for i in 1..=row {
+        for j in 1..=column {
+            condition = (i <= column && j <= i) || (i > column && j <= row - i + 1);
+            if condition {
+                print!("* ")
+            } else {
+                print!("  ")
+            }
+        }
+        println!();
+    }
+
+    println!();
+}
+
+fn pattern12(run: bool) {
+    if !run {
+        return;
+    }
+    println!("-------------------- Pattern 12: --------------------\n");
+
+    let row: i32 = 6;
+    let column: i32 = (row * 2) - 1;
+    let mut condition: bool;
+
+    for i in 1..=row {
+        for j in 1..=column {
+            condition = (j <= row && j >= i) || (j > row && i <= column - j + 1);
+            if condition {
+                print!("* ")
+            } else {
+                print!("  ")
+            }
+        }
+        println!();
+    }
+
+    println!();
+}
+
+fn pattern13(run: bool) {
+    if !run {
+        return;
+    }
+    println!("-------------------- Pattern 13: --------------------\n");
+
+    let row: i32 = 5;
+    let column: i32 = (row * 2) - 1;
+    let mut condition: bool;
+    let mut value: i32; 
+
+    for i in 1..=row {
+        value = i;
+        for j in 1..=column {
+            condition = j >= row + 1 - i && j <= row - 1 + i;
+            if condition {
+                print!("{value} ");
+
+                if j < row {
+                    value += 1;
+                } else if j >= row {
+                    value -= 1;
+                }
+            } else {
+                print!("  ")
+            }
+        }
+        println!();
+    }
+
+    println!();
+}
+
+fn pattern14(run: bool) {
+    if !run {
+        return;
+    }
+    println!("-------------------- Pattern 14: --------------------\n");
+
+    let row: i32 = 7;
+    let column: i32 = row;
+    let mut condition: bool;
+    let mut value: i32;
+
+    for i in 1..=row {
+        value = row - i;
+        for j in 1..=column {
+            condition = j <= row + 1 - i;
+            if condition {
+                print!("{value} ");
+                value -= 1;
+            } else {
+                print!("  ");
+            }
+        }
+        println!();
+    }
+
+    println!();
+}
+
+fn pattern15(run: bool) {
+    if !run {
+        return;
+    }
+
+    let column: i32 = 5;
+    let row: i32 = (column * 2) - 1;
+    let mut condition: bool;
+    let mut value: i32;
+
+    for i in 1..=row {
+        value = 1;
+        for j in 1..=column{
+            condition = i >= column + 1 - j && i <= column - 1 + j;
+            if condition {
+                print!("{value} ");
+                value += 1;
+            } else {
+                print!("  ")
+            }
+        }
+        println!();
+    }
+
+    println!();
+}
+
 fn main() {
     println!("-------------------- Patterns in Rust! --------------------\n\n");
 
@@ -266,4 +404,9 @@ fn main() {
     pattern8(true);
     pattern9(true);
     pattern10(true);
+    pattern11(true);
+    pattern12(true);
+    pattern13(true);
+    pattern14(true);
+    pattern15(true);
 }
