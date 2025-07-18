a82fd080650a81da3ae350413ae87cc803b1a80b
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-07-18 08:26:08 +0530

    f eat: structs methods and associated types

---
+++ the_rust_programming_language_book/structs_methods/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let rectangle1: Rectangle = Rectangle{
+        width: 30,
+        height: 40,
+    };
+
+    let rectangle2: Rectangle = Rectangle{
+        width: 20,
+        height: 30,
+    };
+
+    let rectangle3: Rectangle = Rectangle{
+        width: 10,
+        height: 40,
+    };
+
+    println!("Area of rectangle1 is:- {}", rectangle1.area());
+    println!("Area of rectangle2 is:- {}", rectangle2.area());
+    println!("Area of rectangle3 is:- {}", rectangle3.area());
+
+    println!("Can rect1 hold rect2:- {}", rectangle1.can_hold(&rectangle2));
+    println!("Can rect2 hold rect3:- {}", rectangle2.can_hold(&rectangle3));
+    println!("Can rect1 hold rect3:- {}", rectangle1.can_hold(&rectangle3));
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
+}
